de3a9826560c7518e0a1410958564ecaa25b71af
Piotr Grzela <piotr.grzela@op.pl>
2023-03-26 14:07:42 +0200

    selenium

---
+++ src/test/java/pl/coderslab/SeleniumCourseOnlTeaW35/AssertionsExampleTest.java
@@ -0,0 +1,33 @@
+package pl.coderslab.SeleniumCourseOnlTeaW35;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class AssertionsExampleTest {
+    @Test
+    public void assertionsTest() {
+        // given
+        int a = 2;
+        int b = 40;
+        // when
+        int actual = a + b;
+        // then
+//        if(actual != 42) {
+//            throw new RuntimeException("assertion failed!");
+//        }
+        Assertions.assertTrue(1==1);
+        assertFalse(1==2);
+        assertEquals(42, actual);
+        assertEquals(42, actual, "assertion failed!");
+        assertEquals(3.14, 3.14, 0.01);
+//        assertEquals(0.1 + 0.2, 0.3);
+        assertNotEquals(42, 43);
+        assertNotNull(new Object());
+        assertNull(null);
+        Object o = new Object();
+        assertSame(o, o);
+        assertNotSame(o, new Object());
+    }
+}
+
---
+++ src/test/java/pl/coderslab/SeleniumCourseOnlTeaW35/DuckduckGoTest.java
@@ -0,0 +1,29 @@
+package pl.coderslab.SeleniumCourseOnlTeaW35;
+
+// tes https://duckduckgo.com/ search
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chromium.ChromiumDriver;
+
+class DuckDuckGoTest {
+    @Test
+    public void searchWithDuckDuckGo() {
+        System.setProperty("webdriver.chrome.driver", "/Users/piotrgrzela/homebrew/bin/chromedriver");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver;
+        //driver = new ChromeDriver();
+        driver = new ChromeDriver();
+        driver.get("https://duckduckgo.com/");
+        WebElement searchInput = driver.findElement(By.id("search_form_input_homepage"));
+        searchInput.sendKeys("W pustyni i w puszczy");
+        WebElement searchButton = driver.findElement(By.id("search_button_homepage"));
+        searchButton.click();
+//        driver.close();
+    }
+}
