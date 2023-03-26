c36effe6a172fa51e3471371f5a6ad557acff70e
Piotr Grzela <piotr.grzela@op.pl>
2023-03-26 14:37:13 +0200

    ttt11

--- src/test/java/pl/coderslab/SeleniumCourseOnlTeaW35/DuckduckGoTest.java
+++ src/test/java/pl/coderslab/SeleniumCourseOnlTeaW35/DuckduckGoTest.java
@@ -6,6 +6,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+//import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -24,11 +25,7 @@ class DuckDuckGoTest {
         searchInput.sendKeys("W pustyni i w puszczy");
         WebElement searchButton = driver.findElement(By.id("search_button_homepage"));
         searchButton.click();
-        driver.findElement(By.cssSelector("div#links article h2"));
-        for (int i = 0; i <3; i++)
-            WebElement searchResult = elements.get|(i)
 
-        System.out.println();
 //        driver.close();
     }
 }
